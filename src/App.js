75691472da509ae5ac5ecc1de58a81d22f4ba239
Vivek Patel <visheshpatel228@gmail.com>
2023-04-27 20:13:31 +0530

    Add history button

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 
-function Square({value, onSquareClick}) {
-
-  return(
+function Square({ value, onSquareClick }) {
+  return (
     <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i) {
-    if(squares[i] || calculateWinner(squares)) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
     const nextSquares = squares.slice();
 
-    if(xIsNext) {
+    if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
-
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
   let status;
-  if(winner) {
+  if (winner) {
     status = "Winner : " + winner;
   } else {
     status = "Next Player : " + (xIsNext ? "X" : "O");
@@ -63,21 +58,67 @@ export default function Board() {
 
 function calculateWinner(squares) {
   const lines = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
   ];
-  for(let i = 0; i < lines.length; i++) {
-    const [a,b,c] = lines[i];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
 
-    if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
+  return null;
+}
 
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = (currentMove % 2 === 0);
+  const currentSquare = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map(
+    (squares, move) => {
+      let description;
+
+      if (move > 0) {
+        description = "Move to #" + move;
+      } else {
+        description = "Go to start";
+      }
+
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      );
+    }
+  );
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquare} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 }
